32337a07692dfb519dd4c213f8f595801da4f87f
Chris Michael <chris.perez@upr.edu>
2020-07-18 12:15:18 -0400

    The getTVReviews function has been defined / implemented

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { req } from './utils/index';
 import { BASE_URL } from './urls/index';
 import { GamesParamsOptions } from './interfaces/games'
 import { MoviesParamsOptions } from './interfaces/movies';
+import { TVParamsOptions } from './interfaces/tv';
 
 
 export const getGameReviews = async(options: GamesParamsOptions) =>{
@@ -101,10 +102,91 @@ export const getMoviesReviews = async(options: MoviesParamsOptions) =>{
   return Promise.all(reviews);
 };
 
+export const getTVReviews = async(options: TVParamsOptions) =>{
+  const res = await req(`${BASE_URL}/browse/tv/release-date/${options.filterBy}/${options.sortBy}`);
+  const $ = load(res);
+
+  let reviews = $('body.skybox-auto-collapse div#page table.clamp-list tbody tr').map((_index: number, element: CheerioElement) => 
+    new Promise(async(resolve, _reject) =>{
+        const $element = $(element);
+        const poster = $element.find('td.clamp-image-wrap a img').attr('src') || null;
+        let id = $element.find('td.clamp-summary-wrap a.title').attr('href') || null;
+        const title = $element.find('td.clamp-summary-wrap a.title h3').text() || null;
+        const _score = $element.find('td.clamp-summary-wrap div.clamp-score-wrap a.metascore_anchor div.metascore_w').text() || null;
+        const score = parseInt(_score, 10) || null;
+        const release_date = $element.find('td.clamp-summary-wrap div.clamp-details span').text() || null;
+        const _summary = $element.find('td.clamp-summary-wrap div.summary').text().split('\n' , 2) || null;
+        const summary = (_summary ? (_summary[0].includes('') ? String(_summary[1]).trim() : null) : null) || null;
+
+        if(id === null){
+          resolve({
+            title: title,
+            poster: poster,
+            summary: summary,
+            score: score,
+            release_date: release_date,
+            extra: null
+          });
+        }else{
+          const _id = `${BASE_URL}` + id;
+          const extra = await getTVInfoReview(_id);
+          resolve({
+            title: title,
+            poster: poster,
+            summary: summary,
+            score: score,
+            release_date: release_date,
+            extra: extra
+          });
+        }
+    })
+  ).get();
+  
+  return Promise.all(reviews);
+};
 
 
 
 
+const getTVInfoReview = async(id: string) =>{
+  const res = await req(id);
+  const $ = load(res);
+
+  return new Promise(async(resolve, reject) =>{
+    try{
+      const info = [];
+      $('body.skybox-auto-collapse div#page div#content_header_wrapper div#main_content').each((_index, element) =>{
+        const $element = $(element);
+        let json = JSON.parse($element.find('div.movie script').attr("type","application/ld+json").html()) || null
+        if(json !== null){
+          const numberOfEpisodes = json.numberOfEpisodes || null;
+          const publisher = json.publisher || null;
+          const genre = json.genre || null;
+          const episode = json.episode || null;
+         
+          info.push({
+            numberOfEpisodes: numberOfEpisodes,
+            publisher: publisher,
+            genre: genre,
+            episode: episode
+          });
+        }else{
+          json = null;
+          info.push({});
+        }
+      });      
+
+     
+      const reviews = await getTVCriticReviews(id);
+      const full_review = [{info: info, reviews: reviews}];
+      
+      resolve(full_review);
+
+    }catch(err){
+      reject(err)
+    }
+  });
+}
 
 const getMoviesInfoReview = async(id: string) =>{
   const res = await req(id);
@@ -208,6 +290,34 @@ const getGameInfoReview = async(id: string) =>{
     }
   });
 }
+
+const getTVCriticReviews = async(id: string) =>{
+  const res = await req(`${id}/critic-reviews`);
+  const $ = load(res);
+
+  const reviews = $('body.skybox-auto-collapse div#site_layout div#gutters div#main ol.reviews li.review div.review_content').map((_index, element) =>
+    new Promise((resolve, _reject) =>{
+      try{
+        const $element = $(element);
+        const review_critic = $element.find('div.review_section div.review_stats div.review_critic div.source a').text() || null;
+        const author = $element.find('div.review_section div.review_stats div.review_critic div.author a').text() || null;
+        const review_date = $element.find('div.review_section div.review_stats div.review_critic div.date').text() || null;
+        const review_grade = $element.find('div.review_section div.review_stats div.review_grade div.metascore_w').text() || null;
+        const review = $element.find('div.review_section div.review_body').text().trim() || null;
+        resolve({
+          review: review,
+          review_critic: review_critic,
+          author: author,
+          review_date: review_date,
+          review_grade: review_grade
+        });
+      }catch(err){
+        _reject(err)
+      }
+  })).get();
+
+  return Promise.all(reviews);
+};
 
 const getMoviesCriticReviews = async(id: string) =>{
   const res = await req(`${id}/critic-reviews`);
---
+++ src/interfaces/tv/index.ts
@@ -0,0 +1,18 @@
+
+/**
+ *  Interface TVParamsOptions
+ *  @interface
+ *  @classdesc URL parameter options
+ */
+export interface TVParamsOptions {
+  /**
+  * @member {string} filterBy
+  */
+  filterBy: string,
+  /**
+  * @member {string} sortBy
+  */
+  sortBy: string,
+};
+
+
